1974d32d0c62fdbebd6cc80e019e9e500e92ea49
Andreas <harley1st@hotmail.com>
2015-09-30 15:18:35 +0200

    added bar graph.

--- Evojam/chartJQ.js
+++ Evojam/chartJQ.js
@@ -1,20 +1,34 @@
 $(document).ready(function(){
+	//Global
+	var labelTags = [["Week 47"],["week 48"], ["Week 49"], ["Week 50"], ["Week 51"], ["Week 52"]];
 	
-	
+	var min = Infinity;
+	var dollarSign = function(e) {
+				if (e.value < min)
+					min = e.value;
+		
+				if (e.value === min)
+					return '$';
+				else
+					return e.value;
+			};
+		
+	//Line Chart JQuery
 	
 	// Getting JSON file
-	$.getJSON('data.json', function(data){
-		console.log(data);
+	$.getJSON('dataLine.json', function(dataLine){
+		console.log(dataLine);
 		
 		
 		
-		var chartjsData = [];
-		for (var i = 0; i < data.length; i++) {
-   		chartjsData.push(data[i].data); 
+		//Translate JSON
+		var chartjsDataLine = [];
+		for (var i = 0; i < dataLine.length; i++) {
+   		chartjsDataLine.push(dataLine[i].data); 
 			};
-		//Chart Data
+		//Chart Line Data
 		lineChartData = {
-			labels : [["Week 47"],["week 48"], ["Week 49"], ["Week 50"], ["Week 51"], ["Week 52"]],
+			labels : labelTags,
 			datasets: [
   			{     	 
             label: "Net Comp",
@@ -24,7 +38,7 @@ $(document).ready(function(){
 			pointStrokeColor : "#fff",
 			pointHighlightFill : "#fff",
 			pointHighlightStroke : "#F02626",
-			data : chartjsData[0]},
+			data : chartjsDataLine[0]},
 			{
 			label: "AnalyzerHR",
         	fillColor : "#C2F0FF",
@@ -33,7 +47,7 @@ $(document).ready(function(){
 			pointStrokeColor : "#fff",
 			pointHighlightFill : "#fff",
 			pointHighlightStroke : "#26F041",
-			data : chartjsData[1]},
+			data : chartjsDataLine[1]},
 			{
 			label: "Question Right",
         	fillColor : "#fff",
@@ -42,10 +56,11 @@ $(document).ready(function(){
 			pointStrokeColor : "#fff",
 			pointHighlightFill : "#fff",
 			pointHighlightStroke : "#20AEFA",
-			data : chartjsData[2]}
+			data : chartjsDataLine[2]}
 			
 		]};
 		// Chart Options
+		
 		var options = {
 			scaleShowVerticalLines: false,
 			bezierCurve : false,
@@ -55,18 +70,92 @@ $(document).ready(function(){
 				scaleOverride: true,
 				scaleSteps: 2,
 				scaleStepWidth: 500,
-				scaleStartValue: null,
-				maintainAspectRatio: true
-		}
-		
+				scaleStartValue: 0,
+				maintainAspectRatio: true,
+				 scaleLabel: dollarSign
+		};
 		// Drawing the Chart on the Element in DOM with the id = lineChart
 		var ctx = document.getElementById("lineChart").getContext("2d");
 		var myLineChart = new Chart(ctx).Line(lineChartData, options);
 	
 	});
 	
+
+
 	var revValue = 32.556;
 	$(".revValue").html(revValue);
-
-
+	
+	
+	
+	
+	//Bar Chart JQuery
+	
+	
+	// Getting JSON file
+	$.getJSON('dataBar.json', function(dataBar){
+		console.log(dataBar);
+		
+	//Translate JSON
+		var chartjsDataBar = [];
+		for (var i = 0; i < dataBar.length; i++) {
+   		chartjsDataBar.push(dataBar[i].data); 
+		};
+			
+		BarChartData = {
+			labels : labelTags,
+			datasets: [
+  			{     	 
+            label: "Net Comp",
+        	fillColor : "#F02626",
+			strokeColor : "#F02626",
+  			pointColor : "#F02626",
+			pointStrokeColor : "#fff",
+			pointHighlightFill : "#fff",
+			pointHighlightStroke : "#F02626",
+			data : chartjsDataBar[0]},
+			{
+			label: "AnalyzerHR",
+        	fillColor : "#26F041",
+			strokeColor : "#26F041",
+  			pointColor : "#26F041",
+			pointStrokeColor : "#fff",
+			pointHighlightFill : "#fff",
+			pointHighlightStroke : "#26F041",
+			data : chartjsDataBar[1]},
+			{
+			label: "Question Right",
+        	fillColor : "#20AEFA",
+			strokeColor : "#20AEFA",
+  			pointColor : "#20AEFA",
+			pointStrokeColor : "#fff",
+			pointHighlightFill : "#fff",
+			pointHighlightStroke : "#20AEFA",
+			data : chartjsDataBar[2]}
+			
+		]};
+		
+		var minBar = 1;
+		var options = {
+				scaleShowVerticalLines: false,
+				barShowStroke : false,
+				barValueSpacing : 26,
+    			barDatasetSpacing : 8,
+				scaleOverride: true,
+				scaleSteps: 2,
+				scaleStepWidth: 5,
+				scaleStartValue: 0,
+				maintainAspectRatio: true,
+				 scaleLabel:  function(e) {
+				if (e.value < minBar)
+					minBar = e.value;
+		
+				if (e.value === minBar)
+					return '';
+				else
+					return e.value;
+			}
+		};
+		var ctx2 = document.getElementById("barChart").getContext("2d");
+		var myBarChart = new Chart(ctx2).Bar(BarChartData, options);
+	});
 });
